9111aa065a20ef4f46797b021c6aaf133c0ea58f
puripuri2100 <puripuri2100@gmail.com>
2025-02-21 14:11:16 +0900

    ログを追加し、登録を実装

--- lawscape-register/src/main.rs
+++ lawscape-register/src/main.rs
@@ -6,12 +6,13 @@ use jplaw_data_types::{
     listup::{LawInfo, PrecedentInfo},
     precedent::PrecedentData,
 };
-use lawscape_core::{Law, LegalDocumentsRegistory, Precedent};
+use lawscape_core::{Law, LegalDocument, LegalDocumentsRegistory, Precedent};
 use regex::Regex;
 use std::path::Path;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 use tokio_stream::StreamExt;
+use tracing::info;
 
 fn parse_date(str: &str) -> Result<Date> {
     let re1 = Regex::new("(?<y>[0-9]{4})/(?<m>[0-9]{2})/(?<d>[0-9]{2})").unwrap();
@@ -61,6 +62,15 @@ fn parse_date(str: &str) -> Result<Date> {
     }
 }
 
+/// ログを出力するための設定など
+async fn init_logger() -> Result<()> {
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+    Ok(())
+}
+
 #[derive(Debug, Parser)]
 struct AppArg {
     /// meilisearchのURL
@@ -90,8 +100,14 @@ struct AppArg {
 async fn main() -> Result<()> {
     let app_args = AppArg::parse();
 
+    init_logger().await?;
+
     let law_date = parse_date(&app_args.date)?;
 
+    let legal_document_registory =
+        LegalDocumentsRegistory::new(&app_args.meilisearch_url, &app_args.meilisearch_master_key)?;
+
+    info!("[START] parsing law data");
     // 検索エンジン用の法令データを生成する
     let mut law_index_f = File::open(app_args.law_index).await?;
     let mut law_index_buf = Vec::new();
@@ -117,6 +133,7 @@ async fn main() -> Result<()> {
             }
         }
         if let Some(law_file_name) = patch_file.map(|p| p.to_file_path()) {
+            info!("[START] parsing law: {id_str}");
             let law_file_path = Path::new(&app_args.law_folder);
             let law_file_path = law_file_path.join(&law_file_name);
             let law_file_path = law_file_path.join(format!("{law_file_name}.xml"));
@@ -133,18 +150,24 @@ async fn main() -> Result<()> {
                             .map(|(_, text)| text.clone())
                             .collect::<Vec<String>>()
                             .join("\n");
-                        Law {
+                        LegalDocument::Law(Law {
                             id: id.clone(),
                             name: name.clone(),
                             index: result.article_index.clone(),
                             text,
-                        }
+                        })
                     })
-                    .collect::<Vec<Law>>();
+                    .collect::<Vec<LegalDocument>>();
+            info!("[END] parsing law: {id_str}");
+            info!("[START] register law: {id_str}");
+            legal_document_registory.add_data(&article_list).await?;
+            info!("[END] register law: {id_str}");
         }
     }
+    info!("[END] parsing law data");
 
     // 検索エンジン用の判例データを生成する
+    info!("[START] parsing precedent data");
     let mut precedent_index_f = File::open(app_args.precedent_index).await?;
     let mut precedent_index_buf = Vec::new();
     precedent_index_f
@@ -158,16 +181,20 @@ async fn main() -> Result<()> {
         let mut precedent_buf = Vec::new();
         precedent_file.read_to_end(&mut precedent_buf).await?;
         let precedent = serde_json::from_slice::<PrecedentData>(&precedent_buf)?;
+        info!("[END] parsing precedent: {}", precedent_info.lawsuit_id);
+        info!("[START] register precedent: {}", precedent_info.lawsuit_id);
         if let Some(text) = precedent.contents {
-            let v = [Precedent {
-                id: precedent_info,
+            let v = [LegalDocument::Precedent(Precedent {
+                id: precedent_info.clone(),
                 text,
-            }];
+            })];
+            legal_document_registory.add_data(&v).await?;
         }
+        info!("[END] register precedent: {}", precedent_info.lawsuit_id);
     }
+    info!("[END] parsing precedent data");
 
-    let legal_document_registory =
-        LegalDocumentsRegistory::new(&app_args.meilisearch_url, &app_args.meilisearch_master_key)?;
+    info!("all done");
 
     //TODO
 
